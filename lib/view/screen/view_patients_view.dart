b6565a1a49c181ca182bf9e6ed675e050ac07a3f
Man Nok Pun <sgloriasca@gmail.com>
2023-02-24 17:24:07 -0500

    Styling for View Patients Page

--- lib/view/screen/view_patients_view.dart
+++ lib/view/screen/view_patients_view.dart
@@ -6,6 +6,10 @@ import '../../routes.dart';
 import '../../view_model/screen/view_patients_view_model.dart';
 import '../widget/appBar.dart';
 
+import '../widget/search_patient_icon.dart';
+import '../widget/text.dart';
+import '../widget/textfield.dart';
+
 class ViewPatientsView extends StatefulWidget {
   const ViewPatientsView({Key? key, required this.title}) : super(key: key);
   final String title;
@@ -16,47 +20,86 @@ class ViewPatientsView extends StatefulWidget {
 
 class ViewPatientsViewState
     extends BaseMVVMState<ViewPatientsView, ViewPatientsViewModel> {
+  List<Patient> patients = [];
+  bool isInit = false;
+
+  void onSearch(ViewPatientsViewModel vm, value) {
+    vm.filterPatient(value);
+    setState(() {
+      patients =
+          vm.patients.where((p) => vm.patientHidden[p.id] != true).toList();
+    });
+  }
+
   @override
   Widget buildChild(ctx, ViewPatientsViewModel vm) {
     vm.patients = ModalRoute.of(ctx)!.settings.arguments as List<Patient>;
-
+    if (isInit == false) {
+      Future.delayed(Duration.zero, () async {
+        setState(() {
+          patients = vm.patients;
+          isInit = true;
+        });
+      });
+    }
     return Scaffold(
-      appBar: appBar(
-        "Patient Clinical Data",
-        "View All Patients",
-        [
-          IconButton(
-            onPressed: () => Navigator.pushReplacementNamed(context, Routes.home),
-            icon: const Icon(Icons.home),
-          ),
-          IconButton(
-            onPressed: () => Navigator.pushNamed(context, Routes.addPatient),
-            icon: const Icon(Icons.add),
-          )
-        ],
-      ),
-      body: Column(children: [
-        // SearchBar(),
-        Expanded(
-            child: ListView.builder(
-          itemCount: vm.patients.length,
-          itemBuilder: (context, index) {
-            var patient = vm.patients[index];
-            return ListTile(
-              title: Text(
-                "${patient.bedNumber} ${patient.firstname} ${patient.lastname}",
-                style: const TextStyle(fontSize: 20),
-              ),
-              onTap: () => Navigator.pushNamed(
-                context,
-                Routes.viewPatient,
-                arguments: vm.patients[index],
-              ),
-            );
-          },
-        ))
-      ]),
-    );
+        appBar: appBar(
+          "Patient Clinical Data",
+          "View All Patients",
+          [],
+        ),
+        body: Container(
+            padding: EdgeInsets.only(top: 16),
+            color: Colors.grey[200],
+            child: Column(children: [
+              Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Row(children: [
+                    Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Patient Clinical Data"),
+                          Text("View All Patients"),
+                        ]),
+                    Spacer(),
+                    IconButton(
+                      onPressed: () =>
+                          Navigator.pushReplacementNamed(context, Routes.home),
+                      icon: const Icon(Icons.home),
+                    ),
+                    IconButton(
+                      onPressed: () =>
+                          Navigator.pushNamed(context, Routes.addPatient),
+                      icon: const Icon(Icons.add),
+                    )
+                  ])),
+              Row(children: [
+                searchPatientIcon(ctx),
+                customTextField("Enter Name / Bed Number", "",
+                    (value) => onSearch(vm, value), null),
+              ]),
+
+              // SearchBar(),
+              Expanded(
+                  child: ListView.builder(
+                itemCount: patients.length,
+                itemBuilder: (context, index) {
+                  var patient = patients[index];
+                  return ListTile(
+                    title: Text(
+                      "${patient.bedNumber} ${patient.firstname} ${patient.lastname}",
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                    onTap: () => Navigator.pushNamed(
+                      context,
+                      Routes.viewPatient,
+                      arguments: patients[index],
+                    ),
+                  );
+                },
+              ))
+            ])));
   }
 
   @override
--- lib/view_model/screen/view_patients_view_model.dart
+++ lib/view_model/screen/view_patients_view_model.dart
@@ -3,4 +3,20 @@ import '../../model/patient.dart';
 
 class ViewPatientsViewModel extends BaseViewModel {
   List<Patient> patients = [];
+  Map<String, bool> patientHidden = {};
+
+
+  filterPatient(String input) {
+    if(input.isEmpty) { 
+      patientHidden = {}; 
+      return;
+    }
+    for(Patient p in patients){
+      if( p.bedNumber.toLowerCase().contains( input.toLowerCase())
+       || p.firstname.toLowerCase().contains( input.toLowerCase())
+       || p.lastname.toLowerCase().contains( input.toLowerCase())){
+        patientHidden[p.id] = false;
+      } else { patientHidden[p.id] = true; }
+    }
+  }
 }
