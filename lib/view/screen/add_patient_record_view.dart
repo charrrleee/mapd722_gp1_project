8f7f628db323202297608cdd1b45663e307199a2
Man Nok Pun <sgloriasca@gmail.com>
2023-02-24 20:13:49 -0500

    Styling for add patients record page

--- lib/view/screen/add_patient_record_view.dart
+++ lib/view/screen/add_patient_record_view.dart
@@ -37,130 +37,129 @@ class AddPatientRecordViewState
       ),
     );
   }
+
   final _formKey = GlobalKey<FormState>();
   @override
   Widget buildChild(ctx, AddPatientRecordViewModel vm) {
     vm.patient = ModalRoute.of(ctx)!.settings.arguments as Patient;
 
     return Scaffold(
-      appBar: appBar(
-        "Medical Record",
-        "Add Medical Record",
-        [
-          IconButton(
-            onPressed: () => Navigator.pushReplacementNamed(context, Routes.home),
-            icon: const Icon(Icons.home),
-          ),
-          IconButton(
-            onPressed: () async {
-              if (_formKey.currentState!.validate()) {
-              var response = await vm.savePatientRecord();
-              if (!mounted) return;
-              if (response) {
-                Navigator.pushNamed(
-                  context,
-                  Routes.viewPatientRecords,
-                  arguments: vm.patient,
-                );
-              }
-              }
-            },
-            icon: const Icon(Icons.save),
-          )
-        ],
-      ),
-      body: Form(
-      key: _formKey,
-      child: Column(children: [
-        Row(
-          children: [
-            const Text("Record Date",
-                style: TextStyle(fontSize: 25.0, color: Colors.black)),
-            Expanded(
-              child: CupertinoButton(
-                onPressed: () => _showDialog(
-                  CupertinoDatePicker(
-                    initialDateTime: vm.recordDateTime,
-                    mode: CupertinoDatePickerMode.dateAndTime,
-                    use24hFormat: true,
-                    onDateTimeChanged: vm.onChangeRecordDateTime,
-                  ),
-                ),
-                child: Text(
-                  () {
-                    String convertedDateTime =
-                        "${vm.recordDateTime.year.toString()}-${vm.recordDateTime.month.toString().padLeft(2, '0')}-${vm.recordDateTime.day.toString().padLeft(2, '0')} ${vm.recordDateTime.hour.toString().padLeft(2, '0')}:${vm.recordDateTime.minute.toString().padLeft(2, '0')}";
-                    return convertedDateTime;
-                  }(),
-                  style: const TextStyle(fontSize: 22.0, color: Colors.black),
-                ),
-              ),
-            ),
-          ],
+        appBar: appBar(
+          "Medical Record",
+          "",
+          [],
         ),
-        textFieldStyleWidget(
-          "Blood Pressure",
-          Row(
-            children: [
-              customTextField(
-                "Upper(mmHg)",
-                "",
-                vm.onChangeBloodPressureUpper,
-                vm.validateBP
-              ),
-              customTextField(
-                "Lower(mmHg)",
-                "",
-                vm.onChangeBloodPressureLower,
-                vm.validateBP
-              )
-            ],
-          ),
-        ),
-        textFieldStyleWidget(
-          "Respiratory Rate",
-          Row(
-            children: [
-              customTextField(
-                "rate(/min)",
-                "",
-                vm.onChangeRespiratoryRate,
-              vm.validateRR
-              ),
-            ],
-          ),
-        ),
-        textFieldStyleWidget(
-          "Blood Oxygen Level",
-          Row(
-            children: [
-              customTextField(
-                "level(%)",
-                "",
-                vm.onChangeBloodOxygenLevel,
-              vm.validateBO
-              ),
-            ],
-          ),
-        ),
-        textFieldStyleWidget(
-          "Heartbeat Rate",
-          Row(
-            children: [
-              customTextField(
-                "rate(/min)",
-                "",
-                vm.onChangeHeartBeatRate,
-              vm.validateHBR
-              ),
-            ],
-          ),
-        ),
-        customTextField("Nurse", "", vm.onChangeNurseName,
-              vm.validateEmptyOrNull)
-      ]),
-      )
-    );
+        body: Container(
+            padding: EdgeInsets.only(top: 16),
+            color: Colors.grey[200],
+            child: Column(children: [
+              Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Row(children: [
+                    Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                              "${vm.patient.bedNumber} - ${vm.patient.firstname} ${vm.patient.lastname}"),
+                          Text("Add Medical Record"),
+                        ]),
+                    Spacer(),
+                    IconButton(
+                      onPressed: () =>
+                          Navigator.pushReplacementNamed(context, Routes.home),
+                      icon: const Icon(Icons.home),
+                    ),
+                    IconButton(
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          var response = await vm.savePatientRecord();
+                          if (!mounted) return;
+                          if (response) {
+                            Navigator.pushNamed(
+                              context,
+                              Routes.viewPatientRecords,
+                              arguments: vm.patient,
+                            );
+                          }
+                        }
+                      },
+                      icon: const Icon(Icons.save),
+                    )
+                  ])),
+              Form(
+                  key: _formKey,
+                  child: Expanded(
+                      child: Padding(
+                    padding: EdgeInsets.all(32),
+                    child: Column(children: [
+                      Row(
+                        children: [
+                          const Text("Record Date"),
+                          Expanded(
+                            child: CupertinoButton(
+                              onPressed: () => _showDialog(
+                                CupertinoDatePicker(
+                                  initialDateTime: vm.recordDateTime,
+                                  mode: CupertinoDatePickerMode.dateAndTime,
+                                  use24hFormat: true,
+                                  onDateTimeChanged: vm.onChangeRecordDateTime,
+                                ),
+                              ),
+                              child: Text(
+                                () {
+                                  String convertedDateTime =
+                                      "${vm.recordDateTime.year.toString()}-${vm.recordDateTime.month.toString().padLeft(2, '0')}-${vm.recordDateTime.day.toString().padLeft(2, '0')} ${vm.recordDateTime.hour.toString().padLeft(2, '0')}:${vm.recordDateTime.minute.toString().padLeft(2, '0')}";
+                                  return convertedDateTime;
+                                }(),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      textFieldStyleWidget(
+                        "Blood Pressure",
+                        Row(
+                          children: [
+                            customTextField("Upper(mmHg)", "",
+                                vm.onChangeBloodPressureUpper, vm.validateBP),
+                            customTextField("Lower(mmHg)", "",
+                                vm.onChangeBloodPressureLower, vm.validateBP)
+                          ],
+                        ),
+                      ),
+                      textFieldStyleWidget(
+                        "Respiratory Rate",
+                        Row(
+                          children: [
+                            customTextField("rate(/min)", "",
+                                vm.onChangeRespiratoryRate, vm.validateRR),
+                          ],
+                        ),
+                      ),
+                      textFieldStyleWidget(
+                        "Blood Oxygen Level",
+                        Row(
+                          children: [
+                            customTextField("level(%)", "",
+                                vm.onChangeBloodOxygenLevel, vm.validateBO),
+                          ],
+                        ),
+                      ),
+                      textFieldStyleWidget(
+                        "Heartbeat Rate",
+                        Row(
+                          children: [
+                            customTextField("rate(/min)", "",
+                                vm.onChangeHeartBeatRate, vm.validateHBR),
+                          ],
+                        ),
+                      ),
+                      customTextField("Nurse", "", vm.onChangeNurseName,
+                          vm.validateEmptyOrNull)
+                    ]),
+                  )))
+            ])));
   }
 
   @override
